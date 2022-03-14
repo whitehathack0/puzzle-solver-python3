c0df0415cf74970deda873cc093b0665cdf91d39
Aditya Kumar <akumar@metromile.com>
2022-03-13 23:48:51 -0400

    Made some progress. Need to implement moving methods and figure out why it is not keeping track of path correctly

--- code/puzzlesolver/main/main.py
+++ code/puzzlesolver/main/main.py
@@ -13,12 +13,80 @@ class Node:
         return path_str
 
 
-def bfs(board):
+def is_goal(current_node):
+    length = len(current_node.board)
+
+    for row_index, row in enumerate(current_node.board):
+        for col_index, point in enumerate(row):
+            if row_index*length + col_index != current_node.board[row_index][col_index]:
+                return False
+    return True
+
+
+def go_up(current_node, zero_pos_x, zero_pos_y) -> Node:
+    length = len(current_node.board)
+    move_up_node = current_node
+
+    if zero_pos_x - 1 < 0:
+        return None
+    else:
+        above_val = current_node.board[zero_pos_x-1][zero_pos_y]
+        move_up_node.board[zero_pos_x][zero_pos_y] = above_val
+        move_up_node.board[zero_pos_x - 1][zero_pos_y] = 0
+        return move_up_node
+
+
+def go_down(current_node, zero_pos_x, zero_pos_y) -> Node:
     pass
 
 
+def go_left(current_node, zero_pos_x, zero_pos_y) -> Node:
+    pass
+
+
+def go_right(current_node, zero_pos_x, zero_pos_y) -> Node:
+    pass
+
+
+def bfs(start_node):
+    node_queue = [start_node]
+
+    while node_queue:
+        current_node = node_queue.pop()
+
+        if is_goal(current_node):
+            return current_node.path
+
+        for row_index, row in enumerate(current_node.board):
+            for col_index, point in enumerate(row):
+                if point == 0:
+                    zero_pos_x = row_index
+                    zero_pos_y = col_index
+                    break
+
+        move_up_node = go_up(current_node, zero_pos_x, zero_pos_y)
+        if move_up_node.board:
+            move_up_node.path += current_node.to_string()
+            node_queue.append(move_up_node)
+
+        move_down_node = go_down(current_node, zero_pos_x, zero_pos_y)
+        if move_down_node.board:
+            move_down_node.path += current_node.to_string()
+            node_queue.append(move_down_node)
+
+        move_left_node = go_left(current_node, zero_pos_x, zero_pos_y)
+        if move_left_node.board:
+            move_left_node.path += current_node.to_string()
+            node_queue.append(move_left_node)
+
+        move_right_node = go_right(current_node, zero_pos_x, zero_pos_y)
+        if move_right_node.board:
+            move_right_node.path += current_node.to_string()
+            node_queue.append(move_right_node)
+
+
 if __name__ == '__main__':
-    with open('resources/test.txt') as f:
+    with open('resources/easy_test.txt') as f:
         lines = f.readlines()
         length = len(lines[0].split())
 
@@ -31,5 +99,6 @@ if __name__ == '__main__':
             else:
                 start_board[row_index][col_index] = int(point)
 
-    start_state = Node(start_board, "")
-    goal_state = bfs(start_state)
+    start_state = Node(start_board, '')
+    goal_path = bfs(start_state)
+    print(goal_path)
