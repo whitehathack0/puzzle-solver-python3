6ab7a2a720e6cf7d63f492e0a2cbb3b574e8e1f3
Aditya Kumar <akumar@metromile.com>
2022-03-16 00:24:34 -0400

    Need to figure out why vars are being reset

--- code/puzzlesolver/main/main.py
+++ code/puzzlesolver/main/main.py
@@ -24,7 +24,6 @@ def is_goal(current_node):
 
 
 def go_up(current_node, zero_pos_x, zero_pos_y) -> Node:
-    length = len(current_node.board)
     move_up_node = current_node
 
     if zero_pos_x - 1 < 0:
@@ -37,50 +36,83 @@ def go_up(current_node, zero_pos_x, zero_pos_y) -> Node:
 
 
 def go_down(current_node, zero_pos_x, zero_pos_y) -> Node:
-    pass
+    move_down_node = current_node
+
+    if zero_pos_x + 1 >= len(current_node.board):
+        return None
+    else:
+        below_val = current_node.board[zero_pos_x + 1][zero_pos_y]
+        move_down_node.board[zero_pos_x][zero_pos_y] = below_val
+        move_down_node.board[zero_pos_x + 1][zero_pos_y] = 0
+        return move_down_node
 
 
 def go_left(current_node, zero_pos_x, zero_pos_y) -> Node:
-    pass
+    move_left_node = current_node
+
+    if zero_pos_y - 1 < 0:
+        return None
+    else:
+        left_val = current_node.board[zero_pos_x][zero_pos_y - 1]
+        move_left_node.board[zero_pos_x][zero_pos_y] = left_val
+        move_left_node.board[zero_pos_x][zero_pos_y - 1] = 0
+        return move_left_node
 
 
 def go_right(current_node, zero_pos_x, zero_pos_y) -> Node:
-    pass
+    move_right_node = current_node
+
+    if zero_pos_y + 1 >= len(current_node.board):
+        return None
+    else:
+        right_val = current_node.board[zero_pos_x][zero_pos_y + 1]
+        move_right_node.board[zero_pos_x][zero_pos_y] = right_val
+        move_right_node.board[zero_pos_x][zero_pos_y + 1] = 0
+        return move_right_node
 
 
 def bfs(start_node):
     node_queue = [start_node]
+    counter = 0
 
     while node_queue:
         current_node = node_queue.pop()
+        if counter == 100000:
+            return "1000WASREACHED"
+        else:
+            counter += 1
 
         if is_goal(current_node):
             return current_node.path
 
+        break_out_flag = False
         for row_index, row in enumerate(current_node.board):
             for col_index, point in enumerate(row):
                 if point == 0:
+                    break_out_flag = True
                     zero_pos_x = row_index
                     zero_pos_y = col_index
                     break
+            if break_out_flag:
+                break
 
         move_up_node = go_up(current_node, zero_pos_x, zero_pos_y)
-        if move_up_node.board:
+        if move_up_node and move_up_node.board:
             move_up_node.path += current_node.to_string()
             node_queue.append(move_up_node)
 
         move_down_node = go_down(current_node, zero_pos_x, zero_pos_y)
-        if move_down_node.board:
+        if move_down_node and move_down_node.board:
             move_down_node.path += current_node.to_string()
             node_queue.append(move_down_node)
 
         move_left_node = go_left(current_node, zero_pos_x, zero_pos_y)
-        if move_left_node.board:
+        if move_left_node and move_left_node.board:
             move_left_node.path += current_node.to_string()
             node_queue.append(move_left_node)
 
         move_right_node = go_right(current_node, zero_pos_x, zero_pos_y)
-        if move_right_node.board:
+        if move_right_node and move_right_node.board:
             move_right_node.path += current_node.to_string()
             node_queue.append(move_right_node)
 
